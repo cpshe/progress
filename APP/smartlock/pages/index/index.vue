8388b5bd1e231a1713e4e6500f7e8336667e5c23
xiaoshe <1035513145@qq.com>
2024-12-20 20:11:07 +0800

    提交

--- APP/smartlock/pages/index/index.vue
+++ APP/smartlock/pages/index/index.vue
@@ -18,6 +18,22 @@
 				<switch :checked="beep" @change="onBeepSwitch" />
 			</view>
 
+			<view class="dev-cart">
+				<view class="">
+					<view class="dev-name">生成临时密码</view>
+					<image class="dev-logo" src="../../static/generate.png" mode="/"></image>
+				</view>
+				<switch :checked="generate_flag" @change="onGeneratePwdSwitch" />
+			</view>
+			
+			<view class="dev-cart">
+				<view class="">
+					<view class="dev-name">临时密码</view>
+					<image class="dev-logo" src="../../static/temperarypwd.png" mode="/"></image>
+				</view>
+				<view class="dev-data">{{generate_pwd}}</view>
+			</view>
+			
 		</view>
 	</view>
 </template>
@@ -29,8 +45,10 @@
 	export default {
 		data() {
 			return {
+				generate_pwd: '',
 				led: false,
 				beep: false,
+				generate_flag: false,
 				token: '',
 			}
 		},
@@ -46,9 +64,9 @@
 		},
 		onShow() {
 			this.fetchDevData();
-			// 		setInterval(() => {
-			// 			this.fetchDevData();
-			// 		}, 3000)
+			setInterval(() => {
+				this.fetchDevData();
+			}, 3000)
 		},
 		methods: {
 			fetchDevData() {
@@ -65,7 +83,9 @@
 					success: (res) => {
 						console.log(res.data);
 						this.beep = res.data.data[0].value === 'true';
-						this.led = res.data.data[1].value === 'true';
+						this.generate_flag = res.data.data[2] === 'true';
+						this.generate_pwd = res.data.data[3].value;
+						this.led = res.data.data[4].value === 'true';
 					}
 				});
 			},
@@ -108,6 +128,27 @@
 					},
 					success: () => {
 						console.log('BEEP' + (value ? 'ON' : 'OFF') + '!');
+					}
+				});
+			},
+			onGeneratePwdSwitch(event) {
+				console.log(event.detail.value);
+				let value = event.detail.value;
+				uni.request({
+					url: 'https://iot-api.heclouds.com/thingmodel/set-device-property', //仅为示例，并非真实接口地址。
+					method: 'POST',
+					data: {
+						product_id: '9A7a7Sj8GY',
+						device_name: 'lock',
+						params: {
+							"generate_flag": value,
+						}
+					},
+					header: {
+						'authorization': this.token //自定义请求头信息
+					},
+					success: () => {
+						console.log('GENERATE_PWD' + (value ? 'ON' : 'OFF') + '!');
 					}
 				});
 			}
